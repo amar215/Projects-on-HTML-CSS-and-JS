ee386bc1f6a8bdeae44203427e756e3129fb5ba0
Amar Kosare <amu1234.ak@gmail.com>
2023-04-12 23:37:38 +0530

    Learned a lot about Audio tag of HTML5

---
+++ Project 9 Sound Board/script.js
@@ -0,0 +1,23 @@
+const sounds = ['pipe', 'scream', 'sinister-laugh', 'wow', 'yeah-boy'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    
+    btn.innerText = sound;
+
+    btn.addEventListener('click', ()=> {
+        stopSong();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+})
+
+function stopSong() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
